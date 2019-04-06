13ab84fd046fce612609c589a704946b965b03c8
Anondo <aanondos@gmail.com>
2019-04-06 18:33:06 +0600

    Add app description & logo

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"crontalk/helper"
 	"log"
 
 	"github.com/spf13/cobra"
@@ -10,7 +11,21 @@ import (
 var (
 	RootCmd = &cobra.Command{
 		Use:   "crontalk",
-		Short: "crontalk ...",
+		Short: "Talk to the cron expressions!!",
+		Long: helper.Logo + `
+CronTalk: Talk to the cron expressions!!
+------------------------------------------
+
+Translate your cron expressions into very understandable english words.
+Scan the expression for previous/future occurences.
+
+Valid Cron Expressions:
+-----------------------
+  "* * * * *" = "(minute) (hour) (day of month) (month) (day of week)"
+1.Should contain exactly 5 values/sub-expressions
+2.Values ar only limited to numeric digits for now
+
+		`,
 	}
 )
 
