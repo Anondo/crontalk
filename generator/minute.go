7b8475da9f5ab2d29d2ab76bcfc0539c5800fd48
Anondo <aanondos@gmail.com>
2019-05-29 03:04:06 +0600

    Fix minute generation

--- generator/minute.go
+++ generator/minute.go
@@ -30,20 +30,26 @@ var (
 )
 
 func runMinute() error {
-	cronSlice[minIndex] = ""
+	cronSlice[minIndex] = "" // clearing the default * so that all the new values can be appended
 	var itm string
 	var err error
-	for itm != done {
+	for itm != done { // iterating until the done option is selected because of list values
 		itm, err = helper.RunPrompt("Minute", minItems)
 		if err != nil {
 			return err
 		}
 		if err := parseMinuteExpression(itm); err != nil {
 			fmt.Println(err.Error())
+			continue // to avoid adding unwanted ","
 		}
-		cronSlice[minIndex] += ","
+		if itm != done { //the check is given because this block will be executed once even after done is selected
+			cronSlice[minIndex] += ","
+		}
 	}
-	cronSlice[minIndex] = strings.TrimSuffix(cronSlice[minIndex], ",")
+	cronSlice[minIndex] = strings.TrimSuffix(cronSlice[minIndex], ",") // removing the extra comma at the end
+	if cronSlice[minIndex] == "" {                                     // if done is selected at first, then empty value is replaced by the default *
+		cronSlice[minIndex] = "*"
+	}
 	return nil
 }
 
@@ -54,16 +60,16 @@ func parseMinuteExpression(itm string) error {
 	if itm == input {
 		var min int
 		fmt.Scanf("%d\n", &min)
-		if min < 0 {
-			return errors.New("minute: the minute cannot be a negative number")
+		if min < 0 || min > 59 {
+			return errors.New("minute: the minute cannot be a negative number & cannot be greater than 59")
 		}
 		cronSlice[minIndex] += strconv.Itoa(min)
 	}
 	if itm == defaultStep {
 		var stepVal int
 		fmt.Scanf("%d\n", &stepVal)
-		if stepVal < 1 {
-			return errors.New("minute: the step value cannot be a negative or 0 number")
+		if stepVal < 1 || stepVal > 59 {
+			return errors.New("minute: the step value cannot be a negative or 0 number or greater than 59")
 		}
 		cronSlice[minIndex] += fmt.Sprintf("*/%d", stepVal)
 	}
@@ -71,11 +77,11 @@ func parseMinuteExpression(itm string) error {
 		var val, stepVal int
 		fmt.Scanf("%d\n", &stepVal)
 		fmt.Scanf("%d\n", &val)
-		if stepVal < 1 {
-			return errors.New("minute: the step value cannot be a negative or 0 number")
+		if stepVal < 1 || stepVal > 59 {
+			return errors.New("minute: the step value cannot be a negative or 0 number or greater than 59")
 		}
-		if val < 0 {
-			return errors.New("minute: the minute cannot be a negative number")
+		if val < 0 || val > 59 {
+			return errors.New("minute: the minute cannot be a negative number & cannot be greater than 59")
 		}
 		cronSlice[minIndex] += fmt.Sprintf("%d/%d", val, stepVal)
 	}
@@ -84,7 +90,10 @@ func parseMinuteExpression(itm string) error {
 		fmt.Scanf("%d\n", &from)
 		fmt.Scanf("%d\n", &to)
 		if from < 0 || to < 0 || from > to {
-			return errors.New("the minutes cannot be negative numbers & the from minute must be greater than the to minute")
+			return errors.New("minute: the minutes cannot be negative numbers & the from minute must be greater than the to minute")
+		}
+		if from > 59 || to > 59 {
+			return errors.New("minute: the values cannot be greater than 59")
 		}
 		cronSlice[minIndex] += fmt.Sprintf("%d-%d", from, to)
 	}
@@ -94,11 +103,14 @@ func parseMinuteExpression(itm string) error {
 		fmt.Scanf("%d\n", &stepVal)
 		fmt.Scanf("%d\n", &from)
 		fmt.Scanf("%d\n", &to)
-		if stepVal < 1 {
-			return errors.New("minute: the step value cannot be a negative or 0 number")
+		if stepVal < 1 || stepVal > 59 {
+			return errors.New("minute: the step value cannot be a negative or 0 number or greater than 59")
 		}
 		if from < 0 || to < 0 || from > to {
 			return errors.New("the minutes cannot be negative numbers & the from minute must be greater than the to minute")
+		}
+		if from > 59 || to > 59 {
+			return errors.New("minute: the values cannot be greater than 59")
 		}
 		cronSlice[minIndex] += fmt.Sprintf("%d-%d/%d", from, to, stepVal)
 
